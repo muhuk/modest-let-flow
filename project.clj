7f760ae0b7aa202a3b4636302925810e2897ff94
Atamert Ölçgen <muhuk@muhuk.com>
2015-08-15 14:49:00 +0300

    Incremented version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject modest-let-flow "0.1.0-SNAPSHOT"
+(defproject modest-let-flow "0.1.1-SNAPSHOT"
   :description "Manifold's let-flow with less ambition."
   :url "https://github.com/muhuk/modest-let-flow"
   :license {:name "MIT License"
