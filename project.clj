572ed8098b7529de181ddeae6eb6773e45ac24dd
Atamert Ölçgen <muhuk@muhuk.com>
2016-02-28 11:17:11 +0200

    Upgraded manifold dependency.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/muhuk/modest-let-flow"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[manifold "0.1.1-alpha3"]]
+  :dependencies [[manifold "0.1.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
